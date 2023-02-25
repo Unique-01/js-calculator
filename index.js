4813dd4890900e085555bd8b6d7c4591f5e3b16d
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2023-02-25 09:00:15 +0100

    v1.0 completed

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 let displayBox = document.getElementById('displayBox');
+let resultBox = document.getElementById('resultBox');
+// To diplay the number entered on the screen
 function keyDisplay(e) {
     displayBox.append(e.value)
     displayBox.value += e.value;
     globalThis.displayBoxValue = displayBox.value;
 }
+// To display the computed result on the screen
 function result() {
     let arithemetic = eval(displayBoxValue);
-    displayBox.value = arithemetic;
+    resultBox.innerHTML = arithemetic
 }
+// To clear both input box and the result box
 function clearDisplayBox() {
     displayBox.value="";
+    resultBox.innerHTML="";
+}
+// To delete the last input from the input box
+function deleteInput() {
+    let boxValue = displayBox.value;
+    displayBox.value = boxValue.slice(0,-1);
 }
 
